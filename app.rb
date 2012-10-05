593f9368dd81e152e0c09fea32f41734dcca7304
Sunny Juneja <jr.sunny@gmail.com>
2012-10-04 22:10:46 -0700

    created basic app with db functionality

--- app.rb
+++ app.rb
@@ -3,14 +3,32 @@ require 'sinatra/partial'
 require 'slim'
 require 'sass'
 require 'coffee-script'
+require 'json'
+require 'mongo'
 
 class LandingPages < Sinatra::Base
   configure do
     Slim::Engine.set_default_options :pretty => true
     set :partial_template_engine, :slim
+    conn = Mongo::Connection.new("localhost")
+    db = conn.db("leads")
+    $collection = db.collection("laws")
   end
 
   get '/' do
     slim :index
+  end
+
+  post '/email' do
+    email = { "email" => params[:email] }
+    $collection.insert(email)
+    redirect "emails"
+  end
+
+  get "/emails" do
+    content_type :json
+    @contacts = $collection.find()
+    @results = @contacts.to_a()
+    JSON.dump(@results)
   end
 end
